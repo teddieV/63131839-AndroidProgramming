0ff2c70525d13537cf95430a33556ecff031e162
teddieV <vietanhtran013@gmail.com>
2025-03-07 18:16:21 +0700

    update

---
+++ Ex6_IntentDonGian/app/src/main/java/ntu/tvva/ex6_intentdongian/MainActivityMH3.java
@@ -0,0 +1,20 @@
+package ntu.tvva.ex6_intentdongian;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivityMH3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_mh3);
+       //Tim cac nut dieu khien nut bam
+    }
+}
