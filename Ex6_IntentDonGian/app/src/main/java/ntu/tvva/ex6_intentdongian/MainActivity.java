eec46e3e57d64a59531a5e838f9391a486e10011
teddieV <vietanhtran013@gmail.com>
2025-03-07 18:44:25 +0700

    update bai tap

--- Ex6_IntentDonGian/app/src/main/java/ntu/tvva/ex6_intentdongian/MainActivity.java
+++ Ex6_IntentDonGian/app/src/main/java/ntu/tvva/ex6_intentdongian/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.tvva.ex6_intentdongian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,43 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button nutMH2;
+    Button nutMH3;
+    void Timdieukhien(){
+        nutMH2 = (Button)  findViewById(R.id.btnMH2);
+        nutMH3 = (Button)  findViewById(R.id.btnMH3);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        //Tim cac nut dieu khien nut bam
+        nutMH2 = (Button)  findViewById(R.id.btnMH2);
+        nutMH3 = (Button)  findViewById(R.id.btnMH3);
+        Timdieukhien();
+        //Gan bo lang nghe su kien
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //XU LY CHUYEN MAN HINH
+                // B1: Tao 1 intent(2 tham so 1 ) Man hinh hien tai ; 2 man hinh chuyen toi class
+                Intent intentMH2 = new Intent(MainActivity.this, MainActivityMH2.class);
+                // B2: GUI
+                startActivity(intentMH2);
+            }
+        });
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //XU LY CHUYEN MAN HINH
+                // B1: Tao 1 intent(2 tham so 1 ) Man hinh hien tai ; 2 man hinh chuyen toi class
+                Intent intentMH3 = new Intent(MainActivity.this, MainActivityMH3.class);
+                // B2: GUI
+                startActivity(intentMH3);
+
+            }
         });
     }
+
 }
--- Ex6_IntentDonGian/app/src/main/java/ntu/tvva/ex6_intentdongian/MainActivityMH3.java
+++ Ex6_IntentDonGian/app/src/main/java/ntu/tvva/ex6_intentdongian/MainActivityMH3.java
@@ -1,5 +1,4 @@
 package ntu.tvva.ex6_intentdongian;
-
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,12 +8,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivityMH3 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_mh3);
-       //Tim cac nut dieu khien nut bam
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
