ee7c490b45fc048618b7b9138151d81c4f37508e
teddieV <vietanhtran013@gmail.com>
2025-04-24 02:29:55 +0700

    Tạo file Ex_Fragment Dynamic

--- Ex_THS_FragmentDynamic/app/src/main/java/ntu/tvva/ex_ths_fragmentdynamic/MainActivity.java
+++ Ex_THS_FragmentDynamic/app/src/main/java/ntu/tvva/ex_ths_fragmentdynamic/MainActivity.java
@@ -1,4 +1,4 @@
-package ntu.tvva.ex_fragmentstatic;
+package ntu.tvva.ex_ths_fragmentdynamic;
 
 import android.os.Bundle;
 
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class  MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
